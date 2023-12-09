3ffd3ca3a1cc6a2544a669d9b23f74ae5ac31f7f
Don <100505855+DonFungible@users.noreply.github.com>
2023-12-08 21:28:44 -0800

    sort ipo display

--- components/views/IPOrg/IPOrgDataViewerWrapper.tsx
+++ components/views/IPOrg/IPOrgDataViewerWrapper.tsx
@@ -1,7 +1,7 @@
 import storyClient from '@/lib/SP';
 import React from 'react';
 import IPOrgDataViewerComponent from '@/components/views/IPOrg/IPOrgDataViewerComponent';
-import { IPOrg, ListIPOrgResponse } from '@story-protocol/core-sdk';
+import { ListIPOrgResponse } from '@story-protocol/core-sdk';
 
 export default async function IPOrgDataViewerWrapper({
   tableOnly,
@@ -15,7 +15,7 @@ export default async function IPOrgDataViewerWrapper({
   const data: ListIPOrgResponse = await storyClient.ipOrg.list({});
   const ipOrgData = data?.ipOrgs;
   // crude sort, TODO: make this better
-  ipOrgData.sort((a: IPOrg, b: IPOrg) => parseInt(b.id) - parseInt(a.id));
+  // ipOrgData.sort((a: IPOrg, b: IPOrg) => parseInt(b.id) - parseInt(a.id));
 
   return <IPOrgDataViewerComponent data={ipOrgData} tableOnly={tableOnly} gridOnly={gridOnly} pageSize={pageSize} />;
 }
