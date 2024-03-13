fbdb88db6c85945d86c957d0936a376f9a8bd36b
Allen Chuang <mr.allenchuang@gmail.com>
2024-03-13 14:59:00 -0700

    testnet chain config

--- providers/WagmiConfig.tsx
+++ providers/WagmiConfig.tsx
@@ -3,13 +3,30 @@ import '@rainbow-me/rainbowkit/styles.css';
 
 import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
-import { sepolia } from 'wagmi/chains';
+import { Chain, sepolia } from 'wagmi/chains';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+const testnet = {
+  id: 15_13,
+  name: 'Testnet',
+  // iconUrl: 'https://s2.coinmarketcap.com/static/img/coins/64x64/5805.png',
+  // iconBackground: '#fff',
+  nativeCurrency: { name: 'ETH', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['https://renaissance-testnet.rpc.caldera.xyz/http'] },
+  },
+  blockExplorers: {
+    default: { name: 'BlockScout', url: 'https://renaissance-testnet.explorer.caldera.xyz' },
+  },
+  testnet: true,
+} as const satisfies Chain;
+
+const isTestnet = process.env.NEXT_PUBLIC_CHAIN === 'l2-testnet';
 
 const config = getDefaultConfig({
   appName: 'My RainbowKit App',
   projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID!,
-  chains: [sepolia],
+  chains: isTestnet ? [testnet] : [sepolia],
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
 const queryClient = new QueryClient();
