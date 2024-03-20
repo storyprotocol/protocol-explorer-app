6e457bd2f563a3e00707c04031140d6e73734b0e
Allen Chuang <mr.allenchuang@gmail.com>
2024-03-20 11:40:59 -0700

    update nextjs to 14 and use turbopack

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    forceSwcTransforms: true,
-    serverActions: true,
-  },
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
