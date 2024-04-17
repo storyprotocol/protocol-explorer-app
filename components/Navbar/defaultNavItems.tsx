86a5f4cc411bcaeaf428985acf168aa4078eaac6
Allen Chuang <mr.allenchuang@gmail.com>
2024-04-17 10:39:52 -0700

    remove register for testnet

--- components/Navbar/defaultNavItems.tsx
+++ components/Navbar/defaultNavItems.tsx
@@ -1,4 +1,4 @@
-import { ArrowsRightLeftIcon, RectangleStackIcon, WrenchScrewdriverIcon } from '@heroicons/react/24/outline';
+import { ArrowsRightLeftIcon, RectangleStackIcon } from '@heroicons/react/24/outline';
 import IPIcon from '../icons/IPIcon';
 // define a NavItem prop
 export type NavItem = {
@@ -22,6 +22,7 @@ export const defaultNavItems: NavItem[] = [
     href: '/ipa',
     icon: IPIcon,
   },
+  // TODO: remove register for sepolia until SDK / API is ready
   // {
   //   label: 'Register',
   //   href: '/register',
